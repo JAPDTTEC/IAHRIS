4693dea4bee33b21d154c541b7a3d7dea362d1b5
JAPDTTEC <japd@tragsa.es>
2025-04-25 11:28:45 +0200

    Borrado 3.0

--- Backup/AlterAmbient/FormAcercaDe.vb
+++
@@ -1,6 +0,0 @@
-Public Class FormAcercaDe
-
-    Private Sub btnCerrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCerrar.Click
-        Me.Close()
-    End Sub
-End Class
