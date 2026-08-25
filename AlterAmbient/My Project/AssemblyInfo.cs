5ce0d01f0f426f57831de58a873b796e28df659d
Asier <aalons23@tragsa.es>
2026-08-25 12:35:17 +0200

    cambios 4.2
    arreglo en informes 1bSR

--- AlterAmbient/My Project/AssemblyInfo.cs
+++ AlterAmbient/My Project/AssemblyInfo.cs
@@ -30,12 +30,7 @@ using global::System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")> 
 
-<<<<<<< Updated upstream
-[assembly: AssemblyVersion("4.1.*")]
-[assembly: AssemblyInformationalVersion("4.1")]
-=======
 [assembly: AssemblyVersion("4.2.*")]
 [assembly: AssemblyInformationalVersion("4.2")]
->>>>>>> Stashed changes
 //[assembly: AssemblyFileVersion("2.3.*")]
 
